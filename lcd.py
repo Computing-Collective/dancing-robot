915b1e3c3c2c61004d9f9f47cf79a62d4fd14b7f
Matthew Chow <matthewchow03@gmail.com>
2023-02-01 11:25:21 -0800

    lcd changes

--- lcd.py
+++ lcd.py
@@ -2,6 +2,7 @@
 # https://docs.circuitpython.org/projects/st7789/en/latest/examples.html#x135
 # https://docs.circuitpython.org/projects/st7789/en/latest/
 # https://learn.adafruit.com/making-a-pyportal-user-interface-displayio/groups
+# https://github.com/adafruit/Adafruit_CircuitPython_ST7789/blob/main/examples/st7789_240x135_simpletest_Pimoroni_Pico_Display_Pack.py
 
 import board
 import displayio # https://docs.circuitpython.org/en/latest/shared-bindings/displayio/index.html
@@ -16,27 +17,112 @@ import terminalio # https://docs.circuitpython.org/en/latest/shared-bindings/ter
 from adafruit_st7789 import ST7789 # https://docs.circuitpython.org/projects/st7789/en/latest/
 from adafruit_display_text import label # https://docs.circuitpython.org/projects/display_text/en/latest/api.html#adafruit_display_text.bitmap_label.Label
 
-# First set some parameters used for shapes and text
-BORDER = 20
-FONTSCALE = 2
-BACKGROUND_COLOR = 0x00FF00  # Bright Green
-FOREGROUND_COLOR = 0xAA0088  # Purple
-TEXT_COLOR = 0xFFFF00
+'''
 
-Black = 0x000000
-White = 0xFFFFFF
-Red = 0xFF0000
-Green = 0x00FF00
-Blue = 0x0000FF
-Yellow = 0xFFFF00
-Cyan = 0x00FFFF
-Magenta = 0xFF00FF
-
+Constants
 
 '''
 
-Initialization of SPI for display
+BLACK = 0x000000
+WHITE = 0xFFFFFF
+RED = 0xFF0000
+GREEN = 0x00FF00
+BLUE = 0x0000FF
+YELLOW = 0xFFFF00
+CYAN = 0x00FFFF
+MAGENTA = 0xFF00FF
+ORANGE = 0xFFA500
+INDIGO = 0x4B0082
+VIOLET = 0x8F00FF
 
+BORDER = 20
+BACKGROUND_COLOR = 0x00FF00  # Bright Green 
+FOREGROUND_COLOR = 0xAA0088  # Purple
+TEXT_COLOR = GREEN
+
+FONT_SCALE_SMALL = 1
+FONT_SCALE_MEDIUM = 2
+FONT_SCALE_LARGE = 3
+FONT_SCALE_EXTRA_LARGE = 8 # Full screen height
+
+# Example temp function for displaying basics
+def testDisplay():
+    displayText("o__0", BLACK, display.width // 2, display.height // 2)
+    time.sleep(0.5)
+    clearDisplay()
+    
+    displayText(":)", BLACK, display.width // 2, display.height // 2)  
+    time.sleep(0.5)
+    clearDisplay()
+
+def clearDisplay():
+    while (len(splash) > 1):
+        splash.pop()
+
+def displayDoge():
+    odb = displayio.OnDiskBitmap('/doge.bmp')
+    face = displayio.TileGrid(odb, pixel_shader=odb.pixel_shader)
+    splash.append(face)
+    time.sleep(0.5)
+    clearDisplay()
+
+# Display 
+# "Hello my"
+# "Name is"      ---->    "DIVY"
+def displayIntro():
+    displayText("Hi, my", BLACK, display.width // 2, display.height // 3, FONT_SCALE_LARGE)
+    displayText("name is", BLACK, display.width // 2, 2 * display.height // 3, FONT_SCALE_LARGE)
+    time.sleep(1)
+    clearDisplay()
+    displayText("DIVY", BLACK, display.width // 2, display.height // 2, FONT_SCALE_EXTRA_LARGE)
+    time.sleep(1)
+    clearDisplay()
+
+def displayAsciiFace():
+    pass
+
+def displayColorFace():
+    pass
+
+def displayRainbowStream():
+    color_palette = displayio.Palette(7)
+    color_list =  [RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET]
+    for i in range(len(color_list)):
+        color_palette[i] = color_list[i]
+    
+    color_bitmap2 = displayio.Bitmap((display.width // 7) * 7, display.height, 1)
+    tile_grid = displayio.TileGrid(bitmap = color_bitmap2, pixel_shader = color_palette, tile_width=display.width // 7)
+    
+    
+    splash.append(tile_grid)
+    time.sleep(5)
+    clearDisplay()
+
+    # divide screen into 7 containers
+
+    # shift container values over to the right
+
+'''
+@brief: display a coloured string of text at an (x,y) location on the display
+@param str: the string to print
+@param c: the color of the text (ex: 0xFFFFFF)
+@param x: x location for the text to be displayed. 0 <= x <= 135
+@param y: y location for the text to be displayed. 0 <= y <= 240
+'''
+def displayText(text: str, c, x: int, y: int, font_scale: int):
+    text_area = label.Label(terminalio.FONT, text=text, color=c)
+    text_width = text_area.bounding_box[2] * font_scale
+    text_group = displayio.Group(
+        scale=font_scale,
+        x= x - text_width // 2,
+        y= y,
+    )
+    text_group.append(text_area)
+    splash.append(text_group)
+
+
+'''
+Initialization of SPI for display (global)
 '''
 # Release any resources currently in use for the displays
 displayio.release_displays()
@@ -58,127 +144,37 @@ spi.unlock()
 
 display_bus = displayio.FourWire(spi, command=tft_dc, chip_select=tft_cs, reset=tft_res)
 
-
-'''
-
-Initialization of display
-
-'''
+# Create the display object
 display = ST7789(
     display_bus, rotation=270, width=240, height=135, rowstart=40, colstart=53
 )
 
-
+'''
+Initialization the background (global)
+'''
 # Make the display context
 splash = displayio.Group()
 display.show(splash)
 
 color_bitmap = displayio.Bitmap(display.width, display.height, 1)
-color_palette = displayio.Palette(1)
-color_palette[0] = BACKGROUND_COLOR
 
-bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
+color_pal = displayio.Palette(1)
+color_pal[0] = WHITE
+
+bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_pal, x=0, y=0)
 splash.append(bg_sprite)
-
-# Draw a smaller inner rectangle
-inner_bitmap = displayio.Bitmap(
-    display.width - BORDER * 2, display.height - BORDER * 2, 1
-)
-inner_palette = displayio.Palette(1)
-inner_palette[0] = FOREGROUND_COLOR
-inner_sprite = displayio.TileGrid(
-    inner_bitmap, pixel_shader=inner_palette, x=BORDER, y=BORDER
-)
-splash.append(inner_sprite)
-
-
 
 
 '''
-
-
-# Make the display context
-splash = displayio.Group()
-display.show(splash)
-
-color_bitmap = displayio.Bitmap(135, 240, 1)
-color_palette = displayio.Palette(1)
-color_palette[0] = 0x00FF00
-
-bg_sprite = displayio.TileGrid(color_bitmap,
-                               pixel_shader=color_palette, x=0, y=0)
-splash.append(bg_sprite)
-
-# Draw a smaller inner rectangle
-inner_bitmap = displayio.Bitmap(133, 238, 1)
-inner_palette = displayio.Palette(1)
-inner_palette[0] = 0x0000FF
-inner_sprite = displayio.TileGrid(inner_bitmap,
-                                  pixel_shader=inner_palette, x=1, y=1)
-splash.append(inner_sprite)
-
-# Draw a label
-text_group1 = displayio.Group(max_size=10, scale=2, x=20, y=40)
-text1 = "RPi Pico"
-text_area1 = label.Label(terminalio.FONT, text=text1, color=0xFF0000)
-text_group1.append(text_area1)  # Subgroup for text scaling
-# Draw a label
-text_group2 = displayio.Group(max_size=10, scale=1, x=20, y=60)
-text2 = "CircuitPython"
-text_area2 = label.Label(terminalio.FONT, text=text2, color=0xFFFFFF)
-text_group2.append(text_area2)  # Subgroup for text scaling
-
-# Draw a label
-text_group3 = displayio.Group(max_size=10, scale=1, x=20, y=100)
-text3 = adafruit_st7789.__name__
-text_area3 = label.Label(terminalio.FONT, text=text3, color=0x0000000)
-text_group3.append(text_area3)  # Subgroup for text scaling
-# Draw a label
-text_group4 = displayio.Group(max_size=10, scale=2, x=20, y=120)
-text4 = adafruit_st7789.__version__
-text_area4 = label.Label(terminalio.FONT, text=text4, color=0x000000)
-text_group4.append(text_area4)  # Subgroup for text scaling
-
-splash.append(text_group1)
-splash.append(text_group2)
-splash.append(text_group3)
-splash.append(text_group4)
-
-
+Superloop
 '''
-
-
-# print different lines of text
-text_list = [":)", "hello", "bye"]
-
 while True:
+    print("Code starting")
+    
+    #testDisplay()
 
+    # displayIntro()
 
+    #displayDoge()
 
-    # Output refreshing text
-    text = "0 _ 0"
-    text_area = label.Label(terminalio.FONT, text=text, color=TEXT_COLOR)
-    text_width = text_area.bounding_box[2] * FONTSCALE
-    text_group = displayio.Group(
-        scale=FONTSCALE,
-        x=display.width // 2 - text_width // 2,
-        y=display.height // 2,
-    )
-    text_group.append(text_area)  # Subgroup for text scaling
-    splash.append(text_group)
-    time.sleep(0.5)
-    splash.remove(text_group)
-
-    text = ":)"
-    text_area = label.Label(terminalio.FONT, text=text, color=TEXT_COLOR)
-    text_width = text_area.bounding_box[2] * FONTSCALE
-    text_group = displayio.Group(
-        scale=FONTSCALE,
-        x=display.width // 2 - text_width // 2,
-        y=display.height // 2,
-    )
-    text_group.append(text_area)  # Subgroup for text scaling
-    splash.append(text_group)
-    time.sleep(0.5)
-    splash.pop()
-
+    displayRainbowStream()
