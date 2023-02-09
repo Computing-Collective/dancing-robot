005ca97179a607762d619bc6955327c0f3a08cac
Divy Patel <82171007+divy-07@users.noreply.github.com>
2023-02-08 20:02:17 -0800

    The Reawakening of Motion: A New Era of Timing

--- robot.py
+++ robot.py
@@ -22,7 +22,15 @@ class Robot:
         self.servo_left_lower_foot: servo.Servo = servo_left_lower_foot
         self.servo_right_upper_foot: servo.Servo = servo_right_upper_foot
         self.servo_left_upper_foot: servo.Servo = servo_left_upper_foot
+        
+        # constants
+        self.SLOW_MOVE = int(REFRESH_RATE / 4)
+        self.MEDIUM_MOVE = int(REFRESH_RATE / 8)
+        self.FAST_MOVE = int(REFRESH_RATE / 16)
+        
+        # tracking info
         self.ticks = 0  # Timing counter
+        self.counter = 0  # Counter for changing angles incrementally
         self.move_count = 1  # Move counter
         self.cycles = 0  # Cycle counter for each move
 
@@ -35,13 +43,14 @@ class Robot:
             self._inward_push()
         elif self.move_count == 3:
             self._the_sweep()
-        elif self.move_count == 4:
-            self._balancing_act()
-        elif self.move_count == 5:
-            self._walk_backwards()
-        elif self.move_count == 6:
-            self._move6()
+        # elif self.move_count == 4:
+        #     self._balancing_act()
+        # elif self.move_count == 5:
+        #     self._walk_backwards()
+        # elif self.move_count == 6:
+        #     self._move6()
         self.ticks += 1
+        self.counter += 1
 
     def _wobble(self):
         '''
@@ -49,10 +58,27 @@ class Robot:
         change position every 0.5 seconds
         total cycles = 5
         '''
-        if self.ticks == REFRESH_RATE / 2:
-            self._set_lower_angles(35, 35)
-        elif self.ticks == REFRESH_RATE:
-            self._set_lower_angles(140, 140)
+        speed = self.FAST_MOVE
+        if self.ticks < speed:
+            # lower angles from 90, 90 to 0, 0
+            self._move_to_angles_incrementally(90, 90, 90, 90, 90, 90, 0, 0, self.counter, speed)
+        elif self.ticks == speed:
+            self.counter = 0
+        elif self.ticks < speed * 2:
+            # lower angles from 0, 0 to 90, 90
+            self._move_to_angles_incrementally(90, 90, 0, 0, 90, 90, 90, 90, self.counter, speed)
+        elif self.ticks == speed * 2:
+            self.counter = 0
+        elif self.ticks < speed * 3:
+            # lower angles from 90, 90 to 180, 180
+            self._move_to_angles_incrementally(90, 90, 90, 90, 90, 90, 180, 180, self.counter, speed)
+        elif self.ticks == speed * 3:
+            self.counter = 0
+        elif self.ticks < speed * 4:
+            # lower angles from 180, 180 to 90, 90
+            self._move_to_angles_incrementally(90, 90, 180, 180, 90, 90, 90, 90, self.counter, speed)
+        elif self.ticks == speed * 4:
+            self.counter = 0
             self.cycles += 1
             self.ticks = 0
 
@@ -69,10 +95,17 @@ class Robot:
         change position every 0.5 seconds
         total cycles = 5
         '''
-        if self.ticks == REFRESH_RATE / 2:
-            self._set_lower_angles(5, 170)
-        elif self.ticks == REFRESH_RATE:
-            self._set_lower_angles(90, 90)
+        speed = self.MEDIUM_MOVE
+        if self.ticks < speed:
+            # lower angles from 90, 90 to 0, 180
+            self._move_to_angles_incrementally(90, 90, 90, 90, 90, 90, 0, 180, self.counter, speed)
+        elif self.ticks == speed:
+            self.counter = 0
+        elif self.ticks < speed * 2:
+            # lower angles from 0, 180 to 90, 90
+            self._move_to_angles_incrementally(90, 90, 0, 180, 90, 90, 90, 90, self.counter, speed)
+        elif self.ticks == speed * 2:
+            self.counter = 0
             self.cycles += 1
             self.ticks = 0
 
@@ -89,12 +122,35 @@ class Robot:
         change position every 0.5 seconds
         total cycles = 5
         '''
-        if self.ticks == REFRESH_RATE / 2:
-            self._set_upper_angles(5, 5)
-        elif self.ticks == REFRESH_RATE:
-            self._set_upper_angles(170, 170)
+        speed = self.MEDIUM_MOVE
+        if self.ticks < speed:
+            # upper angles from 90, 90 to 0, 0
+            self._move_to_angles_incrementally(90, 90, 90, 90, 0, 0, 90, 90, self.counter, speed)
+        elif self.ticks == speed:
+            self.counter = 0
+        elif self.ticks < speed * 2:
+            # upper angles from 0, 0 to 90, 90
+            self._move_to_angles_incrementally(0, 0, 90, 90, 90, 90, 90, 90, self.counter, speed)
+        elif self.ticks == speed * 2:
+            self.counter = 0
+        elif self.ticks < speed * 3:
+            # upper angles from 90, 90 to 180, 180
+            self._move_to_angles_incrementally(90, 90, 90, 90, 180, 180, 90, 90, self.counter, speed)
+        elif self.ticks == speed * 3:
+            self.counter = 0
+        elif self.ticks < speed * 4:
+            # upper angles from 180, 180 to 90, 90
+            self._move_to_angles_incrementally(180, 180, 90, 90, 90, 90, 90, 90, self.counter, speed)
+        elif self.ticks == speed * 4:
+            self.counter = 0
             self.cycles += 1
             self.ticks = 0
+        # if self.ticks == REFRESH_RATE / 2:
+        #     self._set_upper_angles(5, 5)
+        # elif self.ticks == REFRESH_RATE:
+        #     self._set_upper_angles(170, 170)
+        #     self.cycles += 1
+        #     self.ticks = 0
 
         # Finish after 5 cycles
         if self.cycles == 5:
@@ -206,6 +262,46 @@ class Robot:
 
     def _move6(self):
         self.move_count += 1
+        
+    def _move_to_angles_incrementally(
+        self,
+        stating_upper_left: int,
+        starting_upper_right: int,
+        starting_lower_left: int,
+        starting_lower_right: int,
+        ending_upper_left: int,
+        ending_upper_right: int,
+        ending_lower_left: int,
+        ending_lower_right: int,
+        local_counter: int,
+        total_ticks: int = None,
+    ):
+        """Moves from starting angles to ending angles incrementally
+
+        @param local_counter: Counter to keep track of how many ticks have passed for this angle change
+                              This is local to the function and should be reset to 0 when the function is called
+                              This is not the same as self.ticks
+                              And this counter needs to be managed by the caller: start with 0, and with every tick, increment by 1
+        @param total_ticks: How many ticks it should take to move from starting to ending angles, default is self.MEDIUM_MOVE
+        """
+        if total_ticks is None:
+            total_ticks = self.MEDIUM_MOVE
+        # Calculate the amount to increment each angle by
+        increment_upper_left = float(ending_upper_left - stating_upper_left) / total_ticks
+        increment_upper_right = float(ending_upper_right - starting_upper_right) / total_ticks
+        increment_lower_left = float(ending_lower_left - starting_lower_left) / total_ticks
+        increment_lower_right = float(ending_lower_right - starting_lower_right) / total_ticks
+        print(increment_lower_left)
+
+        # Calculate the new angles
+        new_upper_left = stating_upper_left + (increment_upper_left * local_counter)
+        new_upper_right = starting_upper_right + (increment_upper_right * local_counter)
+        new_lower_left = starting_lower_left + (increment_lower_left * local_counter)
+        new_lower_right = starting_lower_right + (increment_lower_right * local_counter)
+
+        # Set the new angles
+        self._set_upper_angles(new_upper_left, new_upper_right)
+        self._set_lower_angles(new_lower_left, new_lower_right)
 
     # Servo range is from 5 to 170 for lower servos
     # Servos are individually tuned
