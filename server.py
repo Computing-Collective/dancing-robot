9e536e459781742fde26ce75662fd01d3b8977d6
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2023-02-09 19:06:39 -0800

    Working server code

--- server.py
+++ server.py
@@ -1,15 +1,43 @@
-# Command to run locally
-# get ip with ipconfig and then wlan local area IPv4
-# flask --app server run -h 192.168.137.1
-
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+"""
+For individual move selection
+    -1:  Return to sequence
+    1-6: Specific move
+"""
+move = -1
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello World</p>"
+@app.route('/move', methods=['GET'])
+def get_move(path):
+    """Returns the current move as a string"""
+    global move
+    return str(move)
+
+
+@app.route('/move', methods=['POST'])
+def post_move(path):
+    """Sets the current move"""
+    global move
+    if request.is_json:
+        content = request.json
+        field = content['move']
+        print(f"Received: {field}")
+        if isinstance(field, int) and field >= 1 and field <= 6:
+            move = field
+            return f"Move {field} Sent"
+        elif field == -1:
+            move = field
+            return "Returning to sequence"
+        else:
+            return "Invalid move"
+    else:
+        return "Not JSON"
+
 
 if __name__ == '__main__':
     app.run(port=5000, host="192.168.137.1")
